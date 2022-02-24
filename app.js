f7d7ee8fa616d124c0b82a547da12e923550cd79
Marvin <trujillomarvin@hotmail.com>
2022-02-23 19:37:45 -0800

    working out skeleton

---
+++ app.js
@@ -0,0 +1,18 @@
+log = console.log;
+
+log("hello world");
+
+let myLibrary = [];
+
+function Book() {
+    this.book = book,
+        this.author = author,
+        this.pages = pages,
+        this.read = read
+        // the constructor....
+}
+
+
+function addBookToLibrary() {
+
+}
