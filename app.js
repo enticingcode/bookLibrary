a2bf795160a173d6c70b4e2ec158fa863b2a4933
Marvin <trujillomarvin@hotmail.com>
2022-02-25 15:48:30 -0800

    upload for laptop

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
 log = console.log;
 
-log("hello world");
+// SELECTORS //
+const bookTitle = document.querySelector("#bookTitle");
+const bookAuthor = document.querySelector("#bookAuthor");
+const pages = document.querySelector("#pages");
+const bookCompleted = document.querySelector("#bookCompleted");
+const submitbtn = document.querySelector("#submitbtn");
+
+// log(bookTitle, bookAuthor, pages, bookCompleted, submitbtn);
+
+// log(checkBox.checked);
+// log(bookTitle.value);
+
+
+
+
+/////////////////////////
+
 
 let myLibrary = [];
 
-function Book() {
-    this.book = book,
+function Book(title, author, pages, completed) {
+    this.title = title,
         this.author = author,
         this.pages = pages,
-        this.read = read
-        // the constructor....
+        this.completed = completed
 }
 
 
 function addBookToLibrary() {
 
 }
+
+function addValues() {
+
+}
+
+
+addNewBook = function () {
+    const newBook = new Book(bookTitle.value, bookAuthor.value, pages.value, bookCompleted.checked);
+    myLibrary.push(newBook);
+    log(myLibrary);
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    pages.value = "";
+    bookCompleted.checked = false;
+}
+
+submitbtn.addEventListener("click", addNewBook);
+
+
+
+
+
+
+
+
+log(myLibrary);
