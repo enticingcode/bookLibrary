55a0a437770120ed6473292b7113ee875b957c53
marvo <trujillomarvin@hotmail.com>
2022-02-27 12:59:44 -0800

    adding javascript functionality cards

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const pages = document.querySelector("#pages");
 const bookCompleted = document.querySelector("#bookCompleted");
 const submitbtn = document.querySelector("#submitbtn");
 const library = document.querySelector("#cardContainer");
+const close = document.querySelector("#closeOut");
+
+
+
+// NEW ELEMENT CREATORS //
+
+
+
 
 // log(bookTitle, bookAuthor, pages, bookCompleted, submitbtn);
 
@@ -38,7 +46,7 @@ function addValues() {
 }
 
 
-addNewBook = function () {
+let addNewBook = function () {
     if (bookTitle.value == "") {
         alert("Please insert a book title");
     }
@@ -54,25 +62,67 @@ addNewBook = function () {
     else {
         const newBook = new Book(bookTitle.value, bookAuthor.value, pages.value, bookCompleted.checked);
         myLibrary.push(newBook);
+        addCardStack(bookTitle.value, bookAuthor.value, pages.value, bookCompleted.checked);
         log(myLibrary);
         bookTitle.value = "";
         bookAuthor.value = "";
         pages.value = "";
         bookCompleted.checked = false;
-        displayBooks(myLibrary);
+        // displayBooks(myLibrary);
     }
 }
+let addCardStack = function (title, author, pages, completed) {
+    //  NEW ELEMENTS //
+    const newCard = document.createElement("div");
+    const cardContent = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const authorSpan = document.createElement("span");
+    const pagesSpan = document.createElement("span");
+    const closeButton = document.createElement("span");
 
-function displayBooks(libraryArray) {
-    for (item of libraryArray) {
-        for (obj in item) {
-            log(item[obj]);
-        }
-    }
+
+    // Invokation //
+    newCard;
+    cardContent;
+    closeButton;
+
+    // CLASS NAMING //
+    newCard.className = "card";
+    h3.className = "cardTitle";
+    cardContent.className = "cardContents";
+    authorSpan.className = "cardAuthor";
+    pagesSpan.className = "cardPages";
+    closeButton.className = "closeOut";
+
+    // INNERTEXTS //
+    h3.innerText = `${title}`;
+    authorSpan.innerText = `By: ${author}`;
+    pagesSpan.innerText = `Pages: ${pages}`;
+
+    // APPENDAGES //
+    newCard.append(closeButton);
+    newCard.append(cardContent);
+    cardContent.append(h3);
+    cardContent.append(authorSpan);
+    cardContent.append(pagesSpan);
+
+    library.append(newCard);
+
 }
 
+// function displayBooks(libraryArray) {
+//     for (item of libraryArray) {
+//         for (obj in item) {
+//             log(item[obj]);
+//         }
+//     }
+// }
+
+function deleteCard() {
+    log("itworks")
+}
 submitbtn.addEventListener("click", addNewBook);
-
+closeOut.addEventListener("click", deleteCard);
 
 
 
